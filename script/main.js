8b80f8d125a1694b4b88b0775dee9c3e42f71f18
Tetat Chulchue <61070089@kmitl.ac.th>
2019-02-21 03:04:50 +0700

    Update main.js
    + entity
    + gameLoop
    + load
    + gravity (unfinish)

--- script/main.js
+++ script/main.js
@@ -1,5 +1,62 @@
-import collideDetrctor from collideDetrctor;
+///////////////////////////////
+// Hand-crafted with Love ♥ //
+/////////////////////////////
 
-var canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-var ctx = canvas.getContext("2d"); 
+var player;
+
+const init = {
+    drawCanvas : function draw(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        canvas.width = width;
+        canvas.height = height;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+
+function game() {
+    player.updatePos();
+
+}
+
+function load() {
+    init.drawCanvas(1200, 750, "#383434");
+    player = new entity(400, 100, 50, 80, "#9ce2a0", 0, 0, true, 10);
+    start();
+
+}
+
+function start() {
+    setInterval(game, 33); // 33ms ~ 30fps
+}
+
+function entity(x, y, width, height, color, xVelocity, yVelocity, afftctedByGravity, gValue) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.xVelocity = xVelocity;
+    this.yVelocity = yVelocity;
+    this.afftctedByGravity = afftctedByGravity;
+    this.gValue = gValue;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+
+
+    this.updatePos = function updatePos() {
+        this.y += 10;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        //console.log(this.y);
+        //onsole.log("Test")
+    }
+}
+
+
