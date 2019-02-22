a31a0e143b55aa001bc9f96bac6d48d7f2288894
Tetat Chulchue <61070089@kmitl.ac.th>
2019-02-22 21:58:51 +0700

    Control Code (Unfinished)

--- script/main.js
+++ script/main.js
@@ -6,7 +6,65 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const gForce = 10;
 
-var player;
+var player; //declare player Entity
+
+// ----------------------------- Entity ------------------------------
+
+function entity(id, x, y, width, height, color, xVelocity, yVelocity, state, score) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.xVelocity = xVelocity;
+    this.yVelocity = yVelocity;
+    this.state = state; // there are two state (grounded || airborne)
+    this.score = score;
+
+    this.entityUpdate = function entityUpdate() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    this.updatePos = function updatePos() {
+        if (this.state === "ascending") {
+            if (this.yVelocity === 0) {
+                this.state = "descending";
+            } else {this.yVelocity += 5}
+        }
+        else if (this.state === "descending") {
+            if (yVelocity <= 0) {
+                this.state = "grounded";
+            }
+        }
+
+        this.y += this.yVelocity + gForce;
+        this.x += this.xVelocity;
+
+    }
+
+    this.control = {
+        moveRight : function moveRight() {
+            this.xVelocity += 5;
+        },
+
+        moveLeft : function moveLeft() {
+            this.xVelocity -= 5;
+        },
+
+        jump : function jump() {
+            if (this.state === "grounded") {
+                this.yVelocity -= 20;
+                this.state = "ascending"
+
+            }
+        }
+    }
+
+}
+
+// ------------------ Declare Platform --------------
 
 function platform(x, y, width, height, color) {
     this.x = x;
@@ -17,6 +75,8 @@ function platform(x, y, width, height, color) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
 }
+
+// --------------------- Draw ------------------------
 
 const draw = {
     canvas : function drawCanvas(width, height, color) {
@@ -30,19 +90,19 @@ const draw = {
     },
     map : function drawMap() {
         platform(200, 400, 600, 20, "red");
+        platform(0, 700, 300, 20, "red");
+        platform(750, 600, 100, 20, "red");
+
 
     }
 }
 
+// -------------- Game init --------------------------
+
 function load() {
     draw.canvas(1200, 750, "#383434");
     draw.map();
-    player = new entity(400, 100, 50, 80, "#9ce2a0", 0, 0, "grounded", 0);
-    start();
-
-}
-
-function start() {
+    player = new entity("player", 400, 100, 50, 80, "#9ce2a0", 0, 0, "grounded", 0);
     setInterval(game, 33); // 33ms ~ 30fps
 }
 
@@ -50,33 +110,21 @@ function updateGame() {
     draw.canvas(1200, 750, "#383434"); //render Canvas first
     draw.map(); // than render map
     player.entityUpdate(); // than everything else
+
+    // console.log("x : " + player.x + " | Y : " + player.y);
+    // console.log(player.xVelocity);
 }
 
 function game() {
+    player.control.moveRight();
     player.updatePos();
-    console.log(player.y)
     updateGame();
 
 }
 
-function entity(x, y, width, height, color, xVelocity, yVelocity, state, score) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.xVelocity = xVelocity;
-    this.yVelocity = yVelocity;
-    this.state = state; // there are two state (grounded || airborne)
-    this.score = score;
-    this.entityUpdate = function entityUpdate() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.updatePos = function updatePos() {
-        this.y += gForce;
-    }
 
-}
+
+
+
 
 
