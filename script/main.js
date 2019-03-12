0b27f808ebbdcce66004fdee659b58e9deb6dbf2
Tetat Chulchue <61070089@kmitl.ac.th>
2019-03-13 01:32:26 +0700

    Ammo&Reload, Rename some function ***  PLAYER ARE NOW RAINBOW ***

--- script/main.js
+++ script/main.js
@@ -1,15 +1,12 @@
-/////////////////////////////////////////////////////
-// Hand-crafted Framework with Love and ♥♥♥PAIN♥♥♥ //
-/////////////////////////////////////////////////////
+///////////////////////////////
+// Hand-crafted with Love ♥ //
+/////////////////////////////
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-var player;
-var enemy;
-var enemy2;
+var player, enemy, enemy2, counterJ;
 var level = [];
-var counterJ;
-var statusjump=0;
+var statusjump = 0;
 var faceHit = "R";
 
 
@@ -24,14 +21,14 @@ function Rectangle(x, y, width, height, color) {
 	return this;
 }
 
-function Entity(x, y, width, height, color, hp) {
+function Entity(x, y, width, height, color, hitPoint) {
 	Rectangle.call(this, x, y, width, height, color);
 	this.xVelocity = 0;
 	this.yVelocity = 0;
 	this.score = 0;
 	this.status = 0;
 	this.gForce = 10;
-	this.hp = hp;
+	this.hitPoint = hitPoint;
 	this.ammo = 5;
 
 	this.entityUpdate = function entityUpdate() {
@@ -56,6 +53,10 @@ function Entity(x, y, width, height, color, hp) {
 		this.yVelocity = -20;
 		statusjump = 1;
 	}
+
+	this.reload = () => {
+		this.ammo = 5;
+	}
 	return this;
 }
 
@@ -77,7 +78,7 @@ function enemyMovement(obj){
     }
 }
 
-document.body.addEventListener('keydown', function(e){
+document.body.addEventListener('keydown', (e) => {
 	if(e.code == 'KeyD') {
 		player.moveRight();
 		faceHit = "R";
@@ -87,39 +88,47 @@ document.body.addEventListener('keydown', function(e){
 	} else if (e.code == 'KeyL' && player.status == 1){
 		player.jump();
 		player.status = 0;
-	}else if(e.code == 'KeyK'){
+	} else if (e.code == 'KeyJ') {
+		player.reload();
+		console.log("Reload!");
+	}else if(e.code == 'KeyK' && player.ammo != 0){
 		if(faceHit == "L"){
-			lazer = new Entity(player.x, player.y+(player.height/2)+1, 0-player.x, 5, "red");
-			lazer.updatePos();
-			lazer.entityUpdate();
-			lazer.gForce = 0;
+			laser = new Entity(player.x, player.y+(player.height/2)+1, 0-player.x, 5, "red");
+			laser.gForce = 0;
+			laser.updatePos();
+			laser.entityUpdate();
+			player.ammo -= 1;
 			if(player.y == enemy.y && player.x > enemy.x){
-				enemy.hp = 0;
+				enemy.hitPoint = 0;
 				enemy.y = -100;
 				enemy.gForce = 0;
 			}
 			else if(player.y == enemy2.y && player.x > enemy2.x){
-				enemy2.hp = 0;
+				enemy2.hitPoint = 0;
 				enemy2.y = -100;
 				enemy2.gForce = 0;
 			}
 		}
 		else if(faceHit == "R"){
-			lazer = new Entity(player.x+player.width, player.y+(player.height/2)+1, 1200-player.x, 5, "red");
-			lazer.updatePos();
-			lazer.entityUpdate();
-			lazer.gForce = 0;
+			laser = new Entity(player.x+player.width, player.y+(player.height/2)+1, 1200-player.x, 5, "red");
+			laser.gForce = 0;
+			laser.updatePos();
+			laser.entityUpdate();
+			player.ammo -= 1;
 			if(player.y == enemy.y && player.x < enemy.x){
-				enemy.hp = 0;
+				enemy.hitPoint = 0;
 				enemy.y = -100;
 				enemy.gForce = 0;
 			}
 			else if(player.y == enemy2.y && player.x < enemy2.x){
-				enemy2.hp = 0;
+				enemy2.hitPoint = 0;
 				enemy2.y = -100;
 				enemy2.gForce = 0;
 			}
-}}});
+		}
+		console.log("Ammo Left :", player.ammo);
+	}
+});
 
 document.body.addEventListener('keyup', function(e){
 	if(e.code == 'KeyD' || e.code == 'KeyA') {
@@ -146,7 +155,7 @@ const draw = {
 		
 	}
 }
-function collisionDetector(obj) {
+function collisionDetector(obj) { //Check if player is on platform
 	let pat1 = (((obj.y+obj.height) == level[0].y)&&((obj.x+obj.width >= level[0].x)&&(obj.x <= level[0].x+level[0].width)));
 	let pat2 = (((obj.y+obj.height) == level[1].y)&&((obj.x+obj.width >= level[1].x)&&(obj.x <= level[1].x+level[1].width)));
 	let pat3 = (((obj.y+obj.height) == level[2].y)&&((obj.x+obj.width >= level[2].x)&&(obj.x <= level[2].x+level[2].width)));
@@ -159,7 +168,7 @@ function collisionDetector(obj) {
 		obj.status = 0;
 	}
 }
-function warpPlayer(player){
+function parallax(player){ // Parallax effect
 	if((player.x+player.width < 0)){
 		player.x = 1000;
 	}
@@ -171,7 +180,7 @@ function warpPlayer(player){
 	}
 }
 
-function Jtest(player){ // counterj = Y (before jump)
+function jumpLimit(player){ // counterj = Y (before jump)
 	let j1 = (((player.y+player.height) == level[0].y)&&((player.x+player.width >= level[0].x)&&(player.x <= level[0].x+level[0].width))&&(statusjump == 0));
 	let j2 = (((player.y+player.height) == level[1].y)&&((player.x+player.width >= level[1].x)&&(player.x <= level[1].x+level[1].width))&&(statusjump == 0));
 	let j3 = (((player.y+player.height) == level[2].y)&&((player.x+player.width >= level[2].x)&&(player.x <= level[2].x+level[2].width))&&(statusjump == 0));
@@ -192,7 +201,7 @@ function tophit(){
 		player.jump();
 		player.status = 0;
 		enemy.y = -100;
-		enemy.hp = 0;
+		enemy.hitPoint = 0;
 
 	}
 }
@@ -200,7 +209,7 @@ function tophit(){
 // ---------- Game Loop ----------
 
 function load() {
-    draw.canvas(1200, 750, "#383434");
+    draw.canvas(1000, 500, "#383434");
     draw.map();
     player = new Entity(500, 10, 50, 80, "#9ce2a0", 1);
     enemy = new Entity(300, 50, 50, 80, "pink", 1);
@@ -210,28 +219,29 @@ function load() {
 
 function render() {
     draw.canvas(1000, 500, "#383434"); //render Canvas first
-    draw.map(); // than render map
-    player.entityUpdate(); // than everything else
+	draw.map(); // than render map
+	player.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    player.entityUpdate(); // than render everything else
     enemy.entityUpdate();
     enemy2.entityUpdate();
 }
 
 function game() { //update here
     collisionDetector(player);
-	warpPlayer(player);
-    Jtest(player); // player.y(before jump)
+	parallax(player);
+    jumpLimit(player); // player.y(before jump)
     player.updatePos();   
 	tophit();
-	if(enemy.hp == 1){
+	if(enemy.hitPoint == 1){
 		enemy.updatePos();
 		collisionDetector(enemy);
-		warpPlayer(enemy);
+		parallax(enemy);
 		enemyMovement(enemy);
 	}
-	if(enemy2.hp == 1){
+	if(enemy2.hitPoint == 1){
 		enemy2.updatePos();
 		collisionDetector(enemy2);
-		warpPlayer(enemy2);
+		parallax(enemy2);
 		enemyMovement(enemy2);
 	}
     render();
